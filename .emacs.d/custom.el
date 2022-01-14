f0d410e53e81be812038a460179e2fa6f12642c3
klikflux <42775673+Madtail@users.noreply.github.com>
2022-01-14 16:43:58 +0100

    Create custom.el

---
+++ .emacs.d/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(wombat))
+ '(package-selected-packages '(comment-tags yasnippet omnisharp)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack" :foundry "outline" :slant normal :weight normal :height 98 :width normal))))
+ '(cursor ((t (:background "#40FF40")))))
+
+;; Cursor color
+
+
+
+
+
+(provide 'custom.el);; Custom ends here
