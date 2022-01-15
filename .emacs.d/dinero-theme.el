92462603b2a9190484199066231a40839a7bac36
klikflux <42775673+Madtail@users.noreply.github.com>
2022-01-15 14:25:42 +0100

    Update dinero-theme.el

--- .emacs.d/dinero-theme.el
+++ .emacs.d/dinero-theme.el
@@ -13,7 +13,8 @@
   (dinero-quote-green "#508563")
   (dinero-func-yellow "#F8C212")
   (dinero-var-color "#E3BD3E")
-  (dinero-fileinfo-black "#111418"))
+  (dinero-fileinfo-black "#111418")
+  (dinero-comment-gray "#707070"))
  ;; (dinero-dk-orange  "#eb6123")
  ;; (dinero-purple     "MediumPurple2")
  ;; (dinero-dk-purple  "MediumPurple4")
@@ -21,8 +22,8 @@
 
  ;; Customize faces
  ((default                   (:foreground dinero-main-orange :background dinero-background))
-  (cursor                    (:background dinero-func-yellow))
-  (region                    (:background dinero-main-orange))
+  (cursor                    (:background dinero-white))
+  (region                    (:background dinero-white))
   (mode-line                 (:background dinero-fileinfo-black))
   (font-lock-keyword-face    (:foreground dinero-func-yellow))
   (font-lock-constant-face   (:foreground dinero-func-yellow))
@@ -32,6 +33,7 @@
   (font-lock-variable-name-face     (:foreground dinero-main-orange))
   (font-lock-function-name-face     (:foreground dinero-main-orange))
   (font-lock-type-face              (:foreground dinero-func-yellow))
+  (font-lock-comment-face           (:foreground dinero-comment-gray))
 
   (org-level-1               (:foreground dinero-quote-green))))
 
