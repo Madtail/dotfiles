6d9f4409e738c21b2c3afa8ea1ed676985f79bf3
klikflux <42775673+Madtail@users.noreply.github.com>
2022-01-17 10:40:21 +0100

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,7 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-;; (package-initialize)
+
 ;; ------------------------------------------------------------------------------------------------------------------
 ;;; Code:
 
@@ -73,7 +73,42 @@
                     :width 'normal)
 
 ;;; END of Code ;; ------------------------------------------------------------------------------------------------------------------
+;;; Install packages
+(require 'package)
 
+(add-to-list 'package-archives
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp/")
+
+
+; list the packages you want
+(setq package-list
+      '(yasnippet comment-tags omnisharp autothemer
+       auto-complete flycheck gdscript-mode
+	csharp-mode yaml-mode ac-ispell))
+
+
+; activate all the packages
+(package-initialize)
+
+; fetch the list of packages available 
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+
+;; -----------------------------------------------------
 
 ; csharp
 (defun my-csharp-mode-hook ()
@@ -86,7 +121,7 @@
 (package-install 'omnisharp)
 
 ; auto-complete
-(package-initialize)
+;;(package-initialize)
 (require 'auto-complete)
 (require 'auto-complete-config)
 (ac-config-default)
